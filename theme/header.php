ce7cb7864a640e465ca2b1c0ffb9ef44b2556630
Dan Head <me@danielhead.com>
2018-02-26 17:54:27 +0000

    Adjust spacing

--- theme/header.php
+++ theme/header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
@@ -8,7 +8,7 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway:700" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans|Raleway:700" rel="stylesheet">
 	</head>
 	<body>
   <?php get_template_part('icons') ?>
