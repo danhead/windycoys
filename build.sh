49a5f4691e5831140a49a9b4bf22d1abe487a1b3
Dan Head <me@danielhead.com>
2018-03-07 20:27:58 +0000

    Purge everything

--- build.sh
+++ build.sh
@@ -10,18 +10,9 @@ if [[ -z "${CLOUDFLARE_API_KEY}" ]] || [[ -z "${CLOUDFLARE_EMAIL}" ]] || [[ -z "
   exit 1
 fi
 
-read -r -d '' DATA << EOM
-{
-  "files": [
-    "https://beta.windycoys.com/wp-content/themes/windycoys/style.css",
-    "https://beta.windycoys.com/wp-content/themes/windycoys/bundle.js"
-  ]
-}
-EOM
-
 curl -X DELETE \
   -H "Content-Type: application/json" \
   -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
   -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-  --data "$DATA" \
+  --data '{"purge_everything":true}' \
   "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_IDENTIFIER/purge_cache"
