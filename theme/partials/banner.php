590340e4da97dbcaed54180316d2bf28e00c3f9e
Dan Head <me@danielhead.com>
2019-10-04 22:08:32 +0100

    Add donate shortcode to banner

--- theme/partials/banner.php
+++ theme/partials/banner.php
@@ -1,17 +1,26 @@
 <div class="social">
-  <a class="social__link" href="https://www.facebook.com/WindyCOYS/" aria-label="WindyCOYS on Facebook">
-    <svg class="icon icon--facebook" aria-hidden="true">
-      <use xlink:href="#icon-facebook"></use>
-    </svg>
-  </a>
-  <a class="social__link" href="https://twitter.com/windycoys/" aria-label="WindyCOYS on Twitter">
-    <svg class="icon icon--twitter" aria-hidden="true">
-      <use xlink:href="#icon-twitter"></use>
-    </svg>
-  </a>
-  <a class="social__link" href="/feed" aria-label="WindyCOYS RSS Feed">
-    <svg class="icon icon--rss" aria-hidden="true">
-      <use xlink:href="#icon-rss"></use>
-    </svg>
-  </a>
+  <div class="social__item">
+    <a class="social__link" href="https://www.facebook.com/WindyCOYS/" aria-label="WindyCOYS on Facebook">
+      <svg class="icon icon--facebook" aria-hidden="true">
+        <use xlink:href="#icon-facebook"></use>
+      </svg>
+    </a>
+  </div>
+  <div class="social__item">
+    <a class="social__link" href="https://twitter.com/windycoys/" aria-label="WindyCOYS on Twitter">
+      <svg class="icon icon--twitter" aria-hidden="true">
+        <use xlink:href="#icon-twitter"></use>
+      </svg>
+    </a>
+  </div>
+  <div class="social__item">
+    <a class="social__link" href="/feed" aria-label="WindyCOYS RSS Feed">
+      <svg class="icon icon--rss" aria-hidden="true">
+        <use xlink:href="#icon-rss"></use>
+      </svg>
+    </a>
+  </div>
+  <div class="social__item">
+    <?php echo do_shortcode('[paypal-donation]'); ?>
+  </div>
 </div>
